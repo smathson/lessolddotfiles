bc843f8aae042e2ed382df4de009b22c54e95eef
Scott Mathson <smathson@gmail.com>
2015-03-10 22:11:00 -0700

    [node] replace homebrew node install with nvm

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,5 @@
-if test ! $(which node)
+if [ ! -d "$HOME/.nvm" ]
 then
-  echo "  Installing node for you."
-  brew install node > /tmp/node-install.log
+  echo "  Installing nvm for you."
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | zsh > /tmp/nvm-install.log
 fi
