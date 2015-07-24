1c49ed9e9e7e3c2fd2122c25540e3fe1814580d0
Scott Mathson <smathson@gmail.com>
2015-07-24 09:39:27 -0700

    [zsh] add fix for nom completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# fixes npm completion
+eval "`npm completion`"
